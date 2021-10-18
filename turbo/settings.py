821eee2c790abfab9c661228f61a2a2296c77961
derarsat <74964227+derarsat@users.noreply.github.com>
2021-10-18 14:23:48 +0300

    .

--- turbo/settings.py
+++ turbo/settings.py
@@ -62,18 +62,18 @@ WSGI_APPLICATION = 'turbo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'mysql.connector.django',
-    #     'NAME': 'almoekha_accounting_database',
-    #     'USER': 'almoekha_accounting_user',
-    #     'PASSWORD': 'TITNDPilavm7050@!',
-    #     'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'almoekha_accounting_database',
+        'USER': 'almoekha_accounting_user',
+        'PASSWORD': 'TITNDPilavm7050@!',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
 }
 
 # Password validation
